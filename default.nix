22cf6530de2cee807fbe6f289e2e26e963761a00
huantian1234 <3088054708@qq.com>
2022-11-12 20:42:42 +0800

    add clash-for-windows

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  clash-for-windows=pkgs.callPackage ./pkgs/clash-for-windows {};
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
