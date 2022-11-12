d7351b5e211f7c3373b205e3327672ade4b1e0af
huantian1234 <3088054708@qq.com>
2022-11-12 21:01:13 +0800

    clash

--- pkgs/clash-for-windows/default.nix
+++ pkgs/clash-for-windows/default.nix
@@ -11,15 +11,36 @@
   libappindicator,
   udev,
   at-spi2-atk,
-  pango
+  pango,
+  makeDesktopItem
 }:
-  stdenv.mkDerivation rec{
+let
+  clash-for-windows-icon = {
+  pname = "clash-for-windows-icon";
+  version = "0";
+  src = fetchurl {
+  url = "https://docs.cfw.lbyczf.com/favicon.ico";
+    sha256 = "sha256-4uLJzumIqF6T1yvrdKciqrSNYpJ1+6ecmonRzOsopP0=";
+  };
+  };
+  desktopItem = makeDesktopItem {
+    name = "clash-for-windows";
+    desktopName = "Clash for Windows";
+    comment = "A Windows/macOS/Linux GUI based on Clash and Electron";
+    icon = "clash-for-windows";
+    exec = "cfw";
+    categories = [ "Network" ];
+  };
+  icon = "${clash-for-windows-icon.src}[4]";
+in
+stdenv.mkDerivation rec{
     pname = "clash-for-windows";
     version = "0.20.7";
     src = fetchurl {
       url = "https://ghproxy.com/https://github.com/Fndroid/clash_for_windows_pkg/releases/download/${version}/Clash.for.Windows-${version}-x64-linux.tar.gz";
       sha256 = "sha256-Cf1+b5qtMSnYbdvpC5/A4P0SA9eYAq2gipyv58Wvozo=";
     };
+    
     nativeBuildInputs = [
       autoPatchelfHook
     ];
@@ -48,6 +69,9 @@
       cp -r . "$out/opt/clash-for-windows"
       mkdir -p "$out/bin"
       ln -s "$out/opt/clash-for-windows/cfw" "$out/bin/cfw"
+      
+      mkdir -p "$out/share/applications"
+      install "${desktopItem}/share/applications/"* "$out/share/applications/"
     '';
     meta = with lib; {
       homepage = https://github.com/Fndroid/clash_for_windows_pkg;
